d5485577e1037c201e035a141821db371d59520e
Rajish <rajishav16@gmail.com>
2019-05-04 10:55:26 +0530

    Replaced the car model with a better one

--- Main.gd
+++ Main.gd
@@ -4,4 +4,4 @@ export var car_spawn_height = 10.0
 
 func _ready():
 	var spawn_distance_from_origin = ($GroundGenerator.map_length / 2) * $GroundGenerator.quad_size
-	$Car.global_transform.origin = Vector3(spawn_distance_from_origin, car_spawn_height, spawn_distance_from_origin)
+	$RaceCar.global_transform.origin = Vector3(spawn_distance_from_origin, car_spawn_height, spawn_distance_from_origin)
