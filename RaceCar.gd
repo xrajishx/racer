a8943dfba652a86a948ce3cc28829f3ff409c8f0
Rajish <rajishav16@gmail.com>
2019-05-06 20:46:03 +0530

    Improved the steering

--- RaceCar.gd
+++ RaceCar.gd
@@ -1,20 +1,20 @@
 extends VehicleBody
 
-signal collided_with_checkpoint_marker
-
 export var MAX_ENGINE_FORCE = 200.0
 export var MAX_BRAKE = 10.0
 export var MAX_STEER = 0.5
+
+export var STEER_SPEED = 5.0
 
 func _ready():
 	$AudioStreamPlayer.volume_db = global.default_car_audio_volume
 	$AudioStreamPlayer.pitch_scale = global.default_car_audio_pitch_scale
 
-func _physics_process(_delta):
-	if !$AudioStreamPlayer.playing and linear_velocity.length() * _delta >= 0.0:
+func _physics_process(delta):
+	if !$AudioStreamPlayer.playing and linear_velocity.length() * delta >= 0.0:
 		$AudioStreamPlayer.play()
 
-	$AudioStreamPlayer.pitch_scale = clamp(linear_velocity.length() * _delta / 2, 0.01, 0.6)
+	$AudioStreamPlayer.pitch_scale = clamp(linear_velocity.length() * delta / 2, 0.01, 0.6)
 	var engine_input = 0.0
 	var brake_input = 0.0
 	var steer_input = 0.0
@@ -33,9 +33,14 @@ func _physics_process(_delta):
 
 	engine_force = engine_input * MAX_ENGINE_FORCE
 	brake = brake_input * MAX_BRAKE
-	steering = steer_input * MAX_STEER
 
-func _on_RaceCar_body_shape_entered(_body_id, body, _body_shape, _local_shape):
-	if body.is_in_group('checkpoint'):
-		emit_signal("collided_with_checkpoint_marker")
-		body.queue_free()
+	var steer_change = STEER_SPEED * MAX_STEER * delta
+	if steer_input != 0.0:
+		steering = clamp(steering + (steer_input * steer_change), -MAX_STEER, MAX_STEER)
+	else:
+		if steering > 0.1:
+			steering -= steer_change
+		elif steering < -0.1:
+			steering += steer_change
+		else:
+			steering = 0.0
