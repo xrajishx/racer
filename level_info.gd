17ab76bb498b331d65319ddefe278b6a15d3c2ea
Rajish <rajishav16@gmail.com>
2019-05-12 18:20:51 +0530

    Modified first level

--- level_info.gd
+++ level_info.gd
@@ -2,17 +2,17 @@ extends Node
 
 var levels = [
 	{
-		'seed': 12345,
-		'checkpoints_count': 2,
-		'map_length': 50,
+		'seed': 1,
+		'checkpoints_count': 5,
+		'map_length': 100,
 		'quad_size': 4.0,
 		'height_influence': 10,
 		'tree_counts': [
-			2,
-			2,
-			2,
-			2,
-			2
+			100,
+			100,
+			0,
+			100,
+			0
 		],
 		'noise': {
 			'octaves': 2,
