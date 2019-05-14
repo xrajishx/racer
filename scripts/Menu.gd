bfd7d5c32d29397404c1806bf107b33b562266dd
Rajish <rajishav16@gmail.com>
2019-05-14 22:02:47 +0530

    Fixed a minor issue in the menu screen

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -6,3 +6,6 @@ func _ready():
 func _on_level_selected(level):
 	game_manager.current_level = level
 	helper.load_scene("res://scenes/LevelLoader.tscn")
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
